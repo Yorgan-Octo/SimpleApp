ccf0643b4dc45c69e78889620cd17fdc57974063
Artem Priadchenko <yorgan.info@gmail.com>
2023-08-17 23:23:28 +0300

    Добавьте файлы проекта.

---
+++ SimpleApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult FileDov()
+        {
+
+            byte[] fileBute = System.IO.File.ReadAllBytes("App_Data/data.txt");
+
+            return File(fileBute, "application/txt", "Save All Product.txt");
+        }
+
+
+
+    }
+}
